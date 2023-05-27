d41c22d85302cf0560ac7b09dc57728d427a8ecf
MuhammadSahimBhaur <21100317@lums.edu.pk>
2023-05-27 22:02:55 +0500

    removed setTimeout

--- cv-website/src/hooks/useFetch.js
+++ cv-website/src/hooks/useFetch.js
@@ -5,20 +5,22 @@ const useFetch = (url) => {
     const [data, setData] = useState(null)
     const [error, setError] = useState(null)
 
-    useEffect(() => {
-        setTimeout(
-            () => {
-                fetch(url)
-                    .then(response => {
-                        if (!response.ok) {
-                            throw Error("Could not fetch resource.")
-                        }
-                        return response.json()
-                    })
-                    .then(data => { setData(data) })
-                    .catch((e) => { setError(e.message) })
-            }, 1000)
-    }, [url])
+    useEffect(
+        // () => {
+        // setTimeout(
+        () => {
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error("Could not fetch resource.")
+                    }
+                    return response.json()
+                })
+                .then(data => { setData(data) })
+                .catch((e) => { setError(e.message) })
+            // }, 1000)
+        }, [url]
+    )
 
     return { data, error }
 
