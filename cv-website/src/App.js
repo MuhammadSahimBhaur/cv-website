e2724463a57ef98fbf79aa23104af6ac446ce28f
MuhammadSahimBhaur <21100317@lums.edu.pk>
2023-05-29 23:51:21 +0500

    Using new Hero Comp

--- cv-website/src/App.js
+++ cv-website/src/App.js
@@ -1,20 +1,15 @@
-import Projects from './components/Projects.js';
-import React from 'react';
-import { CssVarsProvider } from '@mui/joy/styles';
-import ResponsiveAppBar from './components/ResponsiveAppBar'
-import Hello from './components/Hello.js';
-import useWindowDimensions from './hooks/useWindowDimensions.js';
+import Projects from "./components/Projects.js";
+import React from "react";
+import { CssVarsProvider } from "@mui/joy/styles";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import Hero from "./components/Hero.js";
+import useWindowDimensions from "./hooks/useWindowDimensions.js";
 import { BrowserRouter } from "react-router-dom";
 
-
-
-
-
 const App = () => {
-
   const windowDimensions = useWindowDimensions();
 
-  console.log(windowDimensions.width, windowDimensions.height)
+  console.log(windowDimensions.width, windowDimensions.height);
 
   return (
     <BrowserRouter>
@@ -23,13 +18,12 @@ const App = () => {
         <ResponsiveAppBar></ResponsiveAppBar>
 
         <div className="two-columns">
-          <Hello />
+          <Hero />
           <Projects />
         </div>
       </div>
     </BrowserRouter>
-
   );
-}
+};
 
 export default App;
