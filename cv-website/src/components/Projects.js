35cd8dce75277c944415bc818a71489e566ddb55
MuhammadSahimBhaur <21100317@lums.edu.pk>
2023-05-29 23:53:01 +0500

    Using FadeIn

--- cv-website/src/components/Projects.js
+++ cv-website/src/components/Projects.js
@@ -1,44 +1,78 @@
 import Project from "./Project";
-import React from 'react';
+import React from "react";
 import useFetch from "../hooks/useFetch";
 
-import Skeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
-
-
-
-
-
+// import Skeleton from "@mui/material/Skeleton";
+// import Stack from "@mui/material/Stack";
+import { FadeIn } from "./FadeIn";
 
 const Projects = () => {
+  let intial_trigger = 0.005;
+  const trigger_interval = 0.1;
 
-    let data = useFetch("http://localhost:3004/projects");
+  let data = useFetch("http://localhost:3004/projects");
 
+  const projects = data.data;
 
-    const projects = data.data
+  // if (projects)
+  //     console.log(data[0])
+  let firstProject;
+  if (projects) {
+    firstProject = projects.shift();
+  }
 
-    if (projects)
-        console.log(data[0])
-
-    return (<div className="projects">
-        { projects ? projects.map((project) => {
-            return <div key={ project.id }><Project data={ project } /></div>
-        }) :
-            <div className="skeleton">
-                <Stack spacing={ 1 }>
-                    <div >
-                        <Skeleton animation="wave" variant="rounded" width={ 700 } height={ 100 } />
-                        <Skeleton animation="wave" variant="text" sx={ { fontSize: '2rem' } } />
-                        <Skeleton animation="wave" variant="text" sx={ { fontSize: '2rem' } } />
-                        <Skeleton animation="wave" variant="text" sx={ { fontSize: '2rem' } } />
-                    </div>
-
-                </Stack>
+  return (
+    <div className="projects">
+      {projects ? (
+        <FadeIn trigger={intial_trigger}>
+          <div key={firstProject.id}>
+            <Project data={firstProject} />
+          </div>
+        </FadeIn>
+      ) : (
+        <></>
+      )}
+      {projects &&
+        projects.map((project) => {
+          return (
+            <FadeIn trigger={(intial_trigger += trigger_interval)}>
+              <div key={project.id}>
+                <Project data={project} />
+              </div>
+            </FadeIn>
+          );
+        })}
+      {/* : (
+        <div className="skeleton">
+          <Stack spacing={1}>
+            <div>
+              <Skeleton
+                animation="wave"
+                variant="rounded"
+                width={700}
+                height={100}
+              />
+              <Skeleton
+                animation="wave"
+                variant="text"
+                sx={{ fontSize: "2rem" }}
+              />
+              <Skeleton
+                animation="wave"
+                variant="text"
+                sx={{ fontSize: "2rem" }}
+              />
+              <Skeleton
+                animation="wave"
+                variant="text"
+                sx={{ fontSize: "2rem" }}
+              />
             </div>
-
-        }
+          </Stack>
+        </div>
+      )} */}
     </div>
-    );
-}
+  );
+};
 
 export default Projects;
