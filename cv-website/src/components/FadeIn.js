8f1ba45282e0b6fbc13e6385dc59eab6f46c270b
MuhammadSahimBhaur <21100317@lums.edu.pk>
2023-05-31 11:33:22 +0500

    Used Props instead of destructering

--- cv-website/src/components/FadeIn.js
+++ cv-website/src/components/FadeIn.js
@@ -1,14 +1,22 @@
 import { animated, useSpring, useScroll } from "@react-spring/web";
 
 export const FadeIn = ({ trigger, children }) => {
+  // Creating an animated div that
+  // has 0 opacity and maximum y allowed in the div
   const [textStyles, textApi] = useSpring(() => ({
     opacity: 0,
     y: "100%",
+    config: {
+      friction: 10,
+      tension: 25,
+    },
   }));
 
+  // Animating on scroll by start animation when
+  // scrollYProgress gets larger than the passed trigger value
   useScroll({
-    onChange: ({ value: { scrollYProgress } }) => {
-      // console.log("scrollYProgress", scrollYProgress);
+    onChange: (props) => {
+      let { scrollYProgress } = props.value;
       if (scrollYProgress > trigger) {
         textApi.start({ opacity: 1, y: "0%" });
       }
@@ -17,10 +25,6 @@ export const FadeIn = ({ trigger, children }) => {
       immediate: false,
     },
   });
-
-  // const styles = useSpring({
-  //   opacity: scrollYProgress,
-  // });
 
   return <animated.div style={textStyles}>{children}</animated.div>;
 };
